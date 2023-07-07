0c541432cba450218ad5172b7a773b25fb24a0fd
Harish Kumar <harishstudio877@gmail.com>
2023-07-07 23:59:27 +0530

    some

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-// import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 import Navbar from './Component/Navbar';
 import Home from './Component/Home';
 import About from './Component/About';
